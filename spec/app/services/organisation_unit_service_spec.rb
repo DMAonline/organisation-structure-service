4ae2dc1d38ce4dab3ac89a16ab245c85da44ae8c
Stephen Robinson <stephen@stephen-robinson.co.uk>
2018-11-16 13:19:38 +0000

    Add get ou and update ou methods to organisation unit service

--- spec/app/services/organisation_unit_service_spec.rb
+++ spec/app/services/organisation_unit_service_spec.rb
@@ -108,4 +108,103 @@ describe OrganisationUnitService do
 
   end
 
+  it 'returns nil if the ou cannot be found by the system and system id' do
+
+    system_id = SecureRandom.uuid
+
+    OrganisationUnit.expects(:where).with(system_id: system_id, system_name: 'pure').returns(stub(:first => nil))
+
+    refute OrganisationUnitService.get_ou_by_system_id 'pure', system_id
+
+  end
+
+  it 'returns instance of organisation unit when ou can be found by system and system id' do
+
+    system_id = SecureRandom.uuid
+
+    OrganisationUnit.expects(:where).with(system_id: system_id, system_name: 'pure').returns(stub(:first => OrganisationUnit.new))
+
+    assert_instance_of OrganisationUnit, OrganisationUnitService.get_ou_by_system_id('pure', system_id)
+
+  end
+
+  it 'returns false if ou with system id doesnot exist' do
+
+    system_id = SecureRandom.uuid
+
+    OrganisationUnitService.expects(:get_ou_by_system_id).with('pure', system_id).returns(nil)
+
+    refute OrganisationUnitService.ou_system_id_exists? 'pure', system_id
+
+  end
+
+  it 'returns true if ou with system id does exist' do
+
+    system_id = SecureRandom.uuid
+
+    OrganisationUnitService.expects(:get_ou_by_system_id).with('pure', system_id).returns(OrganisationUnit.new)
+
+    result = OrganisationUnitService.ou_system_id_exists? 'pure', system_id
+
+    assert result
+
+    assert_equal true, result
+
+  end
+
+  it 'raises invalid ou if org unit cannot be found for update' do
+
+    system_id = SecureRandom.uuid
+
+    OrganisationUnitService.expects(:get_ou_by_system_id).with('pure', system_id).returns(nil)
+
+    params = {
+        system_name: 'pure',
+        system_id: system_id
+    }
+
+    assert_raises InvalidOU do
+      OrganisationUnitService.update_ou params
+    end
+
+  end
+
+  it 'returns false if ou fails to update' do
+
+    org_unit = OrganisationUnit.new
+    system_id = SecureRandom.uuid
+
+    params = {
+        system_name: 'pure',
+        system_id: system_id,
+        name: 'New Name'
+    }
+
+    OrganisationUnitService.expects(:get_ou_by_system_id).with('pure', system_id).returns(org_unit)
+
+    org_unit.expects(:update).with(params).returns(false)
+
+    refute OrganisationUnitService.update_ou params
+
+  end
+
+  it 'returns true when ou updates' do
+
+    org_unit = OrganisationUnit.new
+    system_id = SecureRandom.uuid
+
+    params = {
+        system_name: 'pure',
+        system_id: system_id,
+        name: 'New Name'
+    }
+
+    OrganisationUnitService.expects(:get_ou_by_system_id).with('pure', system_id).returns(org_unit)
+
+    org_unit.expects(:update).with(params).returns(true)
+
+    assert OrganisationUnitService.update_ou params
+
+  end
+
 end
